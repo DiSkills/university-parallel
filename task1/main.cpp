b9f48bd9b9ffdad42c31672d4e8152c6d2f8cf3c
DiSkills <anti.python.fastapi@gmail.com>
2024-09-18 08:03:54 +0300

    Added main

---
+++ task1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "array.hpp"
+#include "matrix.hpp"
+
+
+int main(int argc, char **argv)
+{
+    int n;
+    std::cin >> n;
+
+    srand(time(NULL));
+
+    int **matrix = matrix_generate(n),
+        *array = array_generate(n);
+
+#ifdef DEBUG
+    std::cout << std::endl;
+    matrix_print(matrix, n);
+
+    std::cout << std::endl;
+    array_print(array, n);
+#endif
+
+    matrix_product(matrix, array, n);
+
+    matrix_delete(matrix, n);
+    delete[] array;
+    return 0;
+}
